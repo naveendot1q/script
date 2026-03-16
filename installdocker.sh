f8c6cf311f421d5748e322c9c4804ccf5c544dbc
naveendot1q <naveendot1q@gmail.com>
2026-03-16 15:03:06 +0530

    Update installdocker.sh

--- installdocker.sh
+++ installdocker.sh
@@ -8,9 +8,7 @@ sudo apt install -y apt-transport-https ca-certificates curl software-properties
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 # Add Docker repository
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs)
-
-stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 # Install Docker
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io
